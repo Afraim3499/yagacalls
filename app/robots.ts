205c1880e985afc5d308a71fc54251fbdee20415
Rizwanul Islam <afraim.afraim99@gmail.com>
2026-08-07 11:17:56 +0600

    seo: explicitly allow all major AI search bots (ChatGPT, Claude, Perplexity, Applebot, Google-Extended) in robots.ts

--- app/robots.ts
+++ app/robots.ts
@@ -2,11 +2,31 @@ import { MetadataRoute } from 'next';
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: {
-      userAgent: '*',
-      allow: '/',
-      disallow: ['/api/'],
-    },
+    rules: [
+      {
+        userAgent: '*',
+        allow: '/',
+        disallow: ['/api/'],
+      },
+      {
+        userAgent: [
+          'GPTBot',
+          'ChatGPT-User',
+          'PerplexityBot',
+          'ClaudeBot',
+          'Claude-Web',
+          'Google-Extended',
+          'Googlebot',
+          'Bingbot',
+          'Applebot',
+          'Bytespider',
+          'CCBot',
+          'DuckAssistBot',
+          'PetalBot'
+        ],
+        allow: '/',
+      }
+    ],
     sitemap: 'https://www.yagacalls.com/sitemap.xml',
   };
 }
